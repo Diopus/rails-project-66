c5b539695f51fd84d8605fec3dc611f6f3d1ea0e
diopus <googopus@gmail.com>
2025-04-28 14:44:34 +0200

    Add Sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,8 @@
+Sentry.init do |config|
+  config.dsn = "https://2e50c9d9215df6db31aeee651c446457@o4506713064407040.ingest.us.sentry.io/4509230960279552"
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
+
+  # Add data like request headers and IP for users,
+  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+  config.send_default_pii = true
+end
