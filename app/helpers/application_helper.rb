1c113f8b447b83de5f4a84f5cdbed8674fcf7fb8
diopus <googopus@gmail.com>
2025-04-29 16:10:45 +0200

    Add bootstrap_flash_class helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def bootstrap_flash_class(type)
+    case type
+    when 'notice' then 'alert-primary'
+    when 'alert' then 'alert-danger'
+    when 'success' then 'alert-success'
+    when 'info' then 'alert-info'
+    when 'warning' then 'alert-warning'
+    else 'alert-secondary'
+    end
+  end
 end
