36e16e82a5ce6ec8b8bdf3ae30b666e98c499cb3
diopus <googopus@gmail.com>
2025-04-29 09:00:45 +0200

    Fix linters

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Sentry.init do |config|
-  config.dsn = "https://2e50c9d9215df6db31aeee651c446457@o4506713064407040.ingest.us.sentry.io/4509230960279552"
-  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
+  config.dsn = 'https://2e50c9d9215df6db31aeee651c446457@o4506713064407040.ingest.us.sentry.io/4509230960279552'
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
   # Add data like request headers and IP for users,
   # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
