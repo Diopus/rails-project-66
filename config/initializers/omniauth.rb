c999c2fffcba887127d3dc01ecbcadfc133fdb3b
diopus <googopus@gmail.com>
2025-04-29 10:16:33 +0200

    Fix omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,12 +3,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
     provider :developer
-    github_client_id = GITHUB_CLIENT_ID_DEV
-    github_client_secret = GITHUB_CLIENT_SECRET_DEV
+    github_client_id_var = 'GITHUB_CLIENT_ID_DEV'
+    github_client_secret_var = 'GITHUB_CLIENT_SECRET_DEV'
   else
-    github_client_id = GITHUB_CLIENT_ID
-    github_client_secret = GITHUB_CLIENT_SECRET
+    github_client_id_var = 'GITHUB_CLIENT_ID'
+    github_client_secret_var = 'GITHUB_CLIENT_SECRET'
   end
 
-  provider :github, ENV.fetch(github_client_id, nil), ENV.fetch(github_client_secret, nil), scope: 'user'
+  provider :github, ENV.fetch(github_client_id_var, nil), ENV.fetch(github_client_secret_var, nil), scope: 'user,public_repo,admin:repo_hook'
 end
