3633a9a181f629b74d9ceb671bcba284d8c83788
diopus <googopus@gmail.com>
2025-04-29 14:54:27 +0200

    Set config for i18n

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,8 @@ module RailsProject66
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.available_locales = %i[en ru]
+    config.i18n.default_locale = :en
   end
 end
