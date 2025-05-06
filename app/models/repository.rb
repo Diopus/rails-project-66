605a86365e6dc72d68f80d75596dd5b1bd7e14b6
diopus <googopus@gmail.com>
2025-05-06 21:10:10 +0200

    Add validations, relations and enumerize to repository model

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class Repository < ApplicationRecord
-  belongs_to :user
+  extend Enumerize
+
+  belongs_to :user, inverse_of: :repositories
+
+  validates :github_id, presence: true, uniqueness: true
+
+  enumerize :language, in: [:Ruby] #[:Ruby, :JavaScript]
 end
