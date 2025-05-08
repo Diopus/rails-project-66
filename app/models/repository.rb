e47baac301430fc7ac780b1b8a23048f705f06ee
diopus <googopus@gmail.com>
2025-05-08 20:58:37 +0200

    Add relation to checks in repository

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -4,6 +4,7 @@ class Repository < ApplicationRecord
   extend Enumerize
 
   belongs_to :user, inverse_of: :repositories
+  has_many :checks, class_name: 'Repository::Check', inverse_of: :repository, dependent: :destroy
 
   validates :github_id, presence: true, uniqueness: true
 
