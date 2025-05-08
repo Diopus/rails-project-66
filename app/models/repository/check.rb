41bd65f398402eaedbf8ab245b4623e8862daac8
diopus <googopus@gmail.com>
2025-05-08 20:58:55 +0200

    Add relation to repository in checks

--- app/models/repository/check.rb
+++ app/models/repository/check.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 class Repository::Check < ApplicationRecord
-  belongs_to :repository
+  belongs_to :repository, inverse_of: :checks, touch: true
+  has_many   :offenses,
+             class_name: 'Repository::Offense',
+             dependent: :destroy,
+             inverse_of: :check,
+             counter_cache: :offenses_count
+
+  validates :status, presence: true
 end
