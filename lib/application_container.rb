44594769ec325912d0202f02966a6e2d6e092a7e
diopus <googopus@gmail.com>
2025-05-08 10:36:50 +0200

    Add application container and octokit container

---
+++ lib/application_container.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'dry/container'
+
+class ApplicationContainer
+  extend Dry::Container::Mixin
+
+  register(:octokit_client) do |user_token|
+    if Rails.env.test?
+      OctokitClientStub.new
+    else
+      Octokit::Client.new(access_token: user_token, auto_paginate: true)
+    end
+  end
+end
