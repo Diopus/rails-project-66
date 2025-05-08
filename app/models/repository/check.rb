ecb958aa721bd3706d6dd6263ee57874f523d8b0
diopus <googopus@gmail.com>
2025-05-08 21:07:59 +0200

    Add aasm to Repository::Checks
    Generated with command "rails generate aasm Repository::Check"

--- app/models/repository/check.rb
+++ app/models/repository/check.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Repository::Check < ApplicationRecord
+  include AASM
+
+  aasm do
+  end
   belongs_to :repository, inverse_of: :checks, touch: true
   has_many   :offenses,
              class_name: 'Repository::Offense',
