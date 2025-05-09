f4540281146d2e9652ffbc90d16681474fa8df91
diopus <googopus@gmail.com>
2025-05-09 12:45:59 +0200

    Add aasm events to check model

--- app/models/repository/check.rb
+++ app/models/repository/check.rb
@@ -3,8 +3,6 @@
 class Repository::Check < ApplicationRecord
   include AASM
 
-  aasm do
-  end
   belongs_to :repository, inverse_of: :checks, touch: true
   has_many   :offenses,
              class_name: 'Repository::Offense',
@@ -12,5 +10,24 @@ class Repository::Check < ApplicationRecord
              inverse_of: :check,
              counter_cache: :offenses_count
 
-  validates :status, presence: true
+  aasm whiny_transitions: false do
+    state :pending, initial: true
+    state :cloning, :checking, :finished, :failed
+
+    event :clone_repo do
+      transitions from: :pending, to: :cloning
+    end
+
+    event :check do
+      transitions from: :cloning, to: :checking
+    end
+
+    event :finish do
+      transitions from: :checking, to: :finished
+    end
+
+    event :fail do
+      transitions from: %i[pending cloning checking], to: :failed
+    end
+  end
 end
