7653d7c19f5ce841162f0000b8c7ccca96abef17
diopus <googopus@gmail.com>
2025-05-09 12:46:49 +0200

    Add open3 to container

--- lib/application_container.rb
+++ lib/application_container.rb
@@ -7,7 +7,9 @@ class ApplicationContainer
 
   if Rails.env.test?
     register(:octokit_client) { |_user_token| OctokitClientStub.new }
+    register(:open3) { Open3Stub.new }
   else
     register(:octokit_client) { |user_token| Octokit::Client.new(access_token: user_token, auto_paginate: true) }
+    register(:open3) { Open3 }
   end
 end
