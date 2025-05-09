bd806470ada3aa69c7902b7ec89a90a88de4e432
diopus <googopus@gmail.com>
2025-05-09 13:14:12 +0200

    Add actions to checks_controller

--- app/controllers/web/repositories/checks_controller.rb
+++ app/controllers/web/repositories/checks_controller.rb
@@ -1,7 +1,36 @@
-class Web::Repositories::ChecksController < ApplicationController
-  def show
-  end
+# frozen_string_literal: true
 
-  def create
+module Web
+  module Repositories
+    class ChecksController < ApplicationController
+      before_action :authenticate_user!
+
+      def show
+        @repository = set_repository
+        @check = @repository.checks.find(params[:id])
+
+        authorize @check
+      end
+
+      def create
+        @repository = set_repository
+        authorize @repository, :check?
+
+        @check = @repository.checks.build
+
+        if @check.save
+          CheckRepositoryJob.perform_later(@check.id)
+          redirect_to repository_path(@repository), notice: t('checks.crud.create.success')
+        else
+          redirect_to repository_path(@repository), alert: t('checks.crud.create.failure')
+        end
+      end
+
+      private
+
+      def set_repository
+        @repository = Repository.find(params[:repository_id])
+      end
+    end
   end
 end
