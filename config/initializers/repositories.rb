37d1836b474c648636cd0b28a4493e92f01b2971
diopus <googopus@gmail.com>
2025-05-20 17:19:32 +0200

    Add path configs for linters

--- config/initializers/repositories.rb
+++ config/initializers/repositories.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.config.x.repositories_tmp_path = ENV.fetch('REPOSITORIES_TMP_PATH', 'tmp/repositories')
+Rails.application.config.x.linters.rubocop_config = 'config/linters/rubocop.yml'
+Rails.application.config.x.linters.eslint_config = 'config/linters/eslint.config.mjs'
