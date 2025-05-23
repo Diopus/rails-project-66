30c23896d5f08b3d13588c8ebeb2e61b4c7c7fd8
diopus <googopus@gmail.com>
2025-05-23 17:13:12 +0200

    Make CheckMailer methods

--- app/mailers/repository/check_mailer.rb
+++ app/mailers/repository/check_mailer.rb
@@ -1,13 +1,32 @@
+# frozen_string_literal: true
+
 class Repository::CheckMailer < ApplicationMailer
+  STYLE_GUIDES = {
+    'ruby' => 'https://rubystyle.guide/',
+    'javascript' => 'https://github.com/airbnb/javascript/blob/master/README.md'
+  }.freeze
+
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.repository.check_mailer.check_failed.subject
   #
   def check_failed
-    @greeting = "Hi"
+    @failure_reason = params[:message]
 
-    mail to: "to@example.org"
+    check = params[:check]
+    @check_id = check.id
+
+    repo = check.repository
+    @repo_name = repo.full_name
+    @user = repo.user
+
+    @check_url = repository_check_url(repo, check)
+
+    mail(
+      to: email_address_with_name(@user.email, @user.nickname),
+      subject: t('.subject', repo: @repo_name, check_id: @check_id)
+    )
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -16,8 +35,22 @@ class Repository::CheckMailer < ApplicationMailer
   #   en.repository.check_mailer.offenses_found.subject
   #
   def offenses_found
-    @greeting = "Hi"
+    check = params[:check]
+    @check_id = check.id
+    @count = check.offenses.count
 
-    mail to: "to@example.org"
+    repo = check.repository
+    @repo_name = repo.full_name
+    @user = repo.user
+
+    @check_url = repository_check_url(repo, check)
+
+    key = repo.language.to_s.downcase
+    @style_guide = STYLE_GUIDES[key] or t('.unknown_style_guide')
+
+    mail(
+      to: email_address_with_name(@user.email, @user.nickname),
+      subject: t('.subject', repo: @repo_name, check_id: @check_id)
+    )
   end
 end
