58a1bd9a6f621a816edd4b5d61c7b1ab686829f8
diopus <googopus@gmail.com>
2025-05-23 17:13:37 +0200

    Make CheckMailerPreview methods

--- test/mailers/previews/repository/check_mailer_preview.rb
+++ test/mailers/previews/repository/check_mailer_preview.rb
@@ -1,12 +1,19 @@
+# frozen_string_literal: true
+
 # Preview all emails at http://localhost:3000/rails/mailers/repository/check_mailer
 class Repository::CheckMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/repository/check_mailer/check_failed
   def check_failed
-    Repository::CheckMailer.check_failed
+    Repository::CheckMailer.with(
+      check: Repository::Check.first,
+      message: 'Check failed due to some reason'
+    ).check_failed
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/repository/check_mailer/offenses_found
   def offenses_found
-    Repository::CheckMailer.offenses_found
+    Repository::CheckMailer.with(
+      check: Repository::Check.first
+    ).offenses_found
   end
 end
