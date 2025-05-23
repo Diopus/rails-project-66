5b2162db0b914549d13429f10198f100d05cb91f
diopus <googopus@gmail.com>
2025-05-23 19:09:51 +0200

    Fix linters

--- test/mailers/repository/check_mailer_test.rb
+++ test/mailers/repository/check_mailer_test.rb
@@ -1,19 +1,21 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class Repository::CheckMailerTest < ActionMailer::TestCase
-  test "check_failed" do
+  test 'check_failed' do
     mail = Repository::CheckMailer.check_failed
-    assert_equal "Check failed", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal 'Check failed', mail.subject
+    assert_equal ['to@example.org'], mail.to
+    assert_equal ['from@example.com'], mail.from
+    assert_match 'Hi', mail.body.encoded
   end
 
-  test "offenses_found" do
+  test 'offenses_found' do
     mail = Repository::CheckMailer.offenses_found
-    assert_equal "Offenses found", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal 'Offenses found', mail.subject
+    assert_equal ['to@example.org'], mail.to
+    assert_equal ['from@example.com'], mail.from
+    assert_match 'Hi', mail.body.encoded
   end
 end
