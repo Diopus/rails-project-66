9332a39f12c96cdf02f970f098992883d94438c8
diopus <googopus@gmail.com>
2025-05-25 17:23:58 +0200

    Set cache_store to :redis_cache_store
    ENV BASE_URL used

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'info')
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_cache_store, { url: ENV.fetch('REDIS_URL', nil), expires_in: 12.hours }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :sidekiq
