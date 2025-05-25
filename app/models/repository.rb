ca83a0edae2999cc9396539794485153dd2c034e
diopus <googopus@gmail.com>
2025-05-25 20:14:28 +0200

    Add cache invalidation using after_commit to repository model

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -3,10 +3,24 @@
 class Repository < ApplicationRecord
   extend Enumerize
 
+  REPOSITORIES_LIST_CACHE_KEY_PREFIX = 'github-repos'
+
+  def self.cache_key_for_user_repos(user_id)
+    "#{REPOSITORIES_LIST_CACHE_KEY_PREFIX}/#{user_id}"
+  end
+
+  after_commit :invalidate_cache, on: :create
+
   belongs_to :user, inverse_of: :repositories
   has_many :checks, class_name: 'Repository::Check', inverse_of: :repository, dependent: :destroy
 
   validates :github_id, presence: true, uniqueness: true
 
   enumerize :language, in: %i[Ruby JavaScript]
+
+  private
+
+  def invalidate_cache
+    Rails.cache.delete(self.class.cache_key_for_user_repos(user_id))
+  end
 end
