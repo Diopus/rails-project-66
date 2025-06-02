c60727a2bcd0f29188c77c259c306dfb5f5122e8
diopus <googopus@gmail.com>
2025-06-02 17:54:21 +0200

    Add tests for CheckMailer

--- test/mailers/repository/check_mailer_test.rb
+++ test/mailers/repository/check_mailer_test.rb
@@ -3,19 +3,40 @@
 require 'test_helper'
 
 class Repository::CheckMailerTest < ActionMailer::TestCase
-  test 'check_failed' do
-    mail = Repository::CheckMailer.check_failed
-    assert_equal 'Check failed', mail.subject
-    assert_equal ['to@example.org'], mail.to
-    assert_equal ['from@example.com'], mail.from
-    assert_match 'Hi', mail.body.encoded
+  include Rails.application.routes.url_helpers
+
+  setup do
+    @check = repository_checks(:ruby)
+    @repository = @check.repository
   end
 
-  test 'offenses_found' do
-    mail = Repository::CheckMailer.offenses_found
-    assert_equal 'Offenses found', mail.subject
-    assert_equal ['to@example.org'], mail.to
-    assert_equal ['from@example.com'], mail.from
-    assert_match 'Hi', mail.body.encoded
+  test 'should send check_failed mail when check failed' do
+    mail = Repository::CheckMailer.with(check: @check).check_failed
+    mail_html = mail.html_part.decoded
+    mail_text = mail.text_part.decoded
+    expected_mail_content_values = [@repository.name, @check.id.to_s, repository_check_url(@repository, @check)]
+
+    assert mail.subject.include?(@repository.name)
+    assert mail.to.include?(@repository.user.email)
+
+    expected_mail_content_values.each do |value|
+      assert mail_html.include?(value)
+      assert mail_text.include?(value)
+    end
+  end
+
+  test 'should send offenses_found mail when offenses > 0' do
+    mail = Repository::CheckMailer.with(check: @check).offenses_found
+    mail_text = mail.text_part.decoded
+    mail_html = mail.html_part.decoded
+    expected_mail_content_values = [@repository.name, @check.id.to_s, @check.offenses_count.to_s, repository_check_url(@repository, @check)]
+
+    assert mail.subject.include?(@repository.name)
+    assert mail.to.include?(@repository.user.email)
+
+    expected_mail_content_values.each do |value|
+      assert mail_text.include?(value)
+      assert mail_html.include?(value)
+    end
   end
 end
