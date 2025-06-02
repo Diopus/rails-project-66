6ba9ac45717ac45d0098ae969e288ae25893a81b
diopus <googopus@gmail.com>
2025-06-02 23:35:33 +0200

    Add default "from" to mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV.fetch('MAIL_USERNAME', nil)
+  default from: ENV.fetch('MAIL_USERNAME', 'test')
   layout 'mailer'
 end
