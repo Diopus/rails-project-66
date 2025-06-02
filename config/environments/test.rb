0c63f6abc00cd52e2b3731acd8ed5a4f6a602056
diopus <googopus@gmail.com>
2025-06-02 22:57:04 +0200

    Add 'localhost' to default_url_options[:host] for test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
 
   # Unlike controllers, the mailer instance doesn't have any context about the
   # incoming request so you'll need to provide the :host parameter yourself.
-  config.action_mailer.default_url_options = { host: ENV.fetch('BASE_URL', nil) }
+  config.action_mailer.default_url_options = { host: ENV.fetch('BASE_URL', 'localhost') }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
@@ -66,4 +66,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  Rails.application.routes.default_url_options[:host] = ENV.fetch('BASE_URL', 'localhost')
 end
