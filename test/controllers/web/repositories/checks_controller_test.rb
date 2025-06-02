fd2cb2e5b43ef3347bc02b3bf24274c60132ccda
diopus <googopus@gmail.com>
2025-06-02 17:53:59 +0200

    Add tests for checks_controller

--- test/controllers/web/repositories/checks_controller_test.rb
+++ test/controllers/web/repositories/checks_controller_test.rb
@@ -3,13 +3,43 @@
 require 'test_helper'
 
 class Web::Repositories::ChecksControllerTest < ActionDispatch::IntegrationTest
-  test 'should get show' do
-    get web_repositories_checks_show_url
+  setup do
+    @repository_ruby = repositories(:ruby)
+    @repository_js = repositories(:js)
+    @check_ruby = repository_checks(:ruby)
+    @user = users(:one)
+  end
+
+  test 'should show check' do
+    sign_in @user
+
+    get repository_check_url @check_ruby.repository, @check_ruby
     assert_response :success
   end
 
-  test 'should get create' do
-    get web_repositories_checks_create_url
-    assert_response :success
+  test 'should not show check when not logged in' do
+    get repository_check_url @check_ruby.repository, @check_ruby
+
+    assert_redirected_to root_path
+  end
+
+  test 'should create check for Ruby repository' do
+    sign_in @user
+
+    assert_difference '@repository_ruby.checks.count', 1 do
+      post repository_checks_url @repository_ruby
+    end
+
+    assert_redirected_to repository_url(@repository_ruby)
+    check = @repository_ruby.checks.last
+    assert_enqueued_with(job: CheckRepositoryJob, args: [check.id])
+    assert_equal I18n.t('checks.crud.create.success'), flash[:notice]
+  end
+
+  test 'should not create check for another user repository' do
+    sign_in @user
+
+    post repository_checks_url @repository_js
+    assert_redirected_to root_url
   end
 end
