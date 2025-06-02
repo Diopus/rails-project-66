71ad08223e785c2335c500225094af16adba1040
diopus <googopus@gmail.com>
2025-06-02 17:53:11 +0200

    Add test for webhook

---
+++ test/controllers/api/checks_controller_test.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Api::ChecksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @repository = repositories(:ruby) # используйте фикстуру с github_id
+    @github_id = @repository.github_id
+  end
+
+  test 'should create check and return 200 when repo exists' do
+    assert_difference '@repository.checks.count', 1 do
+      post api_checks_url,
+           params: { repository: { id: @github_id }, after: 'push' },
+           as: :json
+    end
+
+    assert_response :ok
+    assert_enqueued_with(job: CheckRepositoryJob, args: [Repository::Check.last.id])
+  end
+
+  test 'should not create check but return 422 when repo not found' do
+    post api_checks_url,
+         params: { repository: { id: 0 }, after: 'push' },
+         as: :json
+
+    assert_no_enqueued_jobs
+    assert_response :unprocessable_entity
+  end
+end
