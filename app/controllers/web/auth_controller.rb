36a6a63ffa477c0fc14084d26f90ef64c3ed1221
diopus <googopus@gmail.com>
2025-06-12 16:37:02 +0200

    Clean comments

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -25,7 +25,7 @@ module Web
       User.find_or_create_by!(email: auth_hash['info']['email']) do |u|
         u.nickname = auth_hash['info']['nickname']
         u.email = auth_hash['info']['email']
-        u.token = auth_hash['credentials']['token'] # Токен пользователя, потребуется нам позднее
+        u.token = auth_hash['credentials']['token']
       end
     end
   end
