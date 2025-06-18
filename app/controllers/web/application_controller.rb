56e9b05803129c9be520ca703aa5fc9fe9eddf99
diopus <googopus@gmail.com>
2025-06-18 19:34:57 +0200

    Move allow_browser versions: from controllers to controllers/web

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Web::ApplicationController < ApplicationController
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
   before_action :set_locale
 
   include Authentication
